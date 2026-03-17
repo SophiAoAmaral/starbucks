5cf0f09a9f89a0aa1688cc43534af0dae3a07121
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2026-03-16 22:58:35 -0300

    footer arrays

--- starbucks/src/App.jsx
+++ starbucks/src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar"
 import Conteudo from "./components/Conteudo"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <NavBar/>
       <Conteudo/>
+      <Footer/>
     </>
   )
 }
--- starbucks/src/components/Footer.jsx
+++ starbucks/src/components/Footer.jsx
@@ -1,13 +1,31 @@
 function Footer(){
     const links = [
-        {titulo:'Sobre nós', links:['Nossa empresa', 'Nosso café','Atendimento ao cliente','Canal de Denúncias', 'Código de Conduta Zamp']},
-        {titulo:'Carreira', links:['Trabalhe Conosco']},
-        {titulo:'Impacto Social', links:['Comunidade','Meio Ambiente','Fornecimento ético','História Starbucks ®']},
+        {titulo:'Sobre nós', textos:['Nossa empresa', 'Nosso café','Atendimento ao cliente','Canal de Denúncias', 'Código de Conduta Zamp']},
+        {titulo:'Carreira', textos:['Trabalhe Conosco']},
+        {titulo:'Impacto Social', textos:['Comunidade','Meio Ambiente','Fornecimento ético','História Starbucks ®']},
+    ];
 
-    ]
+    const politicas =['Diretrizes de privacidade de dados','Política de troca de produto', 'Termos de uso', 'Opção de privacidade'];
     return(
-        <section>
+        <footer className= " flex justify-center font-primary border-t-1 mx-auto gap-30 mt-20 p-10 border-gray-300">
+            {
+            links.map((link) =>(
+                <div>
+                <h1 href="/" className="text-2xl mb-2.5">{link.titulo}</h1>
+                <ul>
+                    {
+                        link.textos.map((texto)=>(
+                            <li className="mb-4 text-black/95 font-light hover:text-green-950 hover:font-semibold text-base"><a className="underline" href="">{texto}</a></li>
+                        ))
+                    }
+                </ul>
+                </div>
+            ))
             
-        </section>
+        }
+        <img src="../public/img/image.png" alt=""  className="mr-4 w-40 h-25  cursor-pointer"/>
+        
+        </footer>
     )
 }
+export default Footer
