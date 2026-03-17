73123eccb094ce827e415111c0b74db497fb856a
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2026-03-16 21:56:49 -0300

    footer

--- starbucks/src/components/Conteudo.jsx
+++ starbucks/src/components/Conteudo.jsx
@@ -8,7 +8,7 @@ function Conteudo(){
                 </div>
                 <div className="verde-escuro mb-9">
                     <div className="flex flex-col text-center mt-30">
-                    <h1 className="text-3xl  uppercase text-white font-bold mb-3">Cardápio</h1>
+                    <h1 className="text-4xl  uppercase text-white font-bold mb-3">Cardápio</h1>
                     <p className="mb-3 text-lg text-white">Confira abaixo tudo o que a STARBUCKS® oferece:</p>
                     <a href="">
                         <button className="bg-white rounded-full py-2 px-4 w-60 mx-auto mt-2 text-green-800 font-semibold cursor-pointer">Cardápio Digital* <svg xmlns="http://www.w3.org/2000/svg" className="inline" height="19px" viewBox="0 -960 960 960" width="17px" fill="#016630"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg></button>
@@ -17,7 +17,7 @@ function Conteudo(){
                 </div>
                 <div className="grid bg-green-50 ">
                     <div className="flex flex-col text-center mt-30">
-                        <h1 className="font-black text-3xl uppercase mb-3">Tabela Nutricional</h1>
+                        <h1 className="font-black text-4xl uppercase mb-3">Tabela Nutricional</h1>
                         <p className="mb-3 text-lg ">Veja todas as informações dos nossos produtos</p>
                         <div className="mt-2">
                             <button className="mr-4 rounded-full py-2 px-5 bg-green-700 text-white font-semibold hover:bg-green-900 transition-all duration-300">Bebidas <svg  xmlns="http://www.w3.org/2000/svg" className="inline" height="19px" viewBox="0 -960 960 960" width="17px" fill="#ffff"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg></button>
---
+++ starbucks/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+    const links = [
+        {titulo:'Sobre nós', links:['Nossa empresa', 'Nosso café','Atendimento ao cliente','Canal de Denúncias', 'Código de Conduta Zamp']},
+        {titulo:'Carreira', links:['Trabalhe Conosco']},
+        {titulo:'Impacto Social', links:['Comunidade','Meio Ambiente','Fornecimento ético','História Starbucks ®']},
+
+    ]
+    return(
+        <section>
+            
+        </section>
+    )
+}
